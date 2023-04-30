c37e9135f59bea740f40da9b9f03b32f37b304b4
shyleshragu <48572160+shyleshragu@users.noreply.github.com>
2023-04-30 11:46:41 -0400

    Add files via upload
    added before pc reset

---
+++ src/main/java/ca/jrvs/apps/CacheRuntime2.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Main {
+
+    public static class Cache {}
+
+    public static class DiskCache extends Cache {}
+
+    public static class MemoryCache extends Cache {}
+
+    public static class OptimizeddiskCache extends DiskCache {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        //good
+//        OptimizeddiskCache optimizeddiskCache1 = new OptimizeddiskCache();
+//        Cache cache1 = (Cache) optimizeddiskCache1;
+
+        //Exception
+//        MemoryCache memoryCache2 = new MemoryCache();
+//        Cache cache2 = (Cache) memoryCache2;
+//        DiskCache diskCache2 = (DiskCache) cache2;
+//
+        //Exception
+//        DiskCache diskCache3 = new DiskCache();
+//        OptimizeddiskCache optimizeddiskCache3 = (OptimizeddiskCache) diskCache3;
+//
+        //Good
+//        OptimizeddiskCache optimizeddiskCache4 = new OptimizeddiskCache();
+//        DiskCache diskCache4 = (DiskCache) optimizeddiskCache4;
+
+        //Exception
+//        Cache cache5 = new Cache();
+//        MemoryCache memoryCache5 = (MemoryCache) cache5;
+//
+        //Good
+//        OptimizeddiskCache optimizeddiskCache6 = new OptimizeddiskCache();
+//        Cache cache6 = (Cache) optimizeddiskCache6;
+//        DiskCache diskCache6 = (DiskCache) cache6;
+    }
+}
